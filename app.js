6b620b0fbcfdd999287e0ed77def4085ed427805
tamim-hmizi <hmizitamim@hotmail.com>
2024-07-01 14:04:09 +0100

    production ready

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ require('dotenv').config()
 const indexRouter = require('./routes/index')
 const itemRouter = require('./routes/item')
 const categoryRouter = require('./routes/category')
-
+const compression = require('compression')
 const mongoose = require('mongoose')
-
+const helmet = require('helmet')
 const app = express()
+
+const RateLimit = require('express-rate-limit')
+const limiter = RateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 20,
+})
 
 mongoose.set('strictQuery', false)
 
@@ -25,10 +31,19 @@ async function main() {
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+app.use(limiter)
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(compression())
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            'script-src': ["'self'", 'code.jquery.com', 'cdn.jsdelivr.net'],
+        },
+    })
+)
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
